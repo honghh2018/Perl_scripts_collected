381c4267a975bf6b48301d4bdec1bbe67a8bad5a
Future <41668708+honghh2018@users.noreply.github.com>
2018-08-30 14:52:52 +0800

    Create blast_result_abstract_3_generate_transcriptomic.pl

---
+++ blast_result_abstract_3_generate_transcriptomic.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/perl -w
+use strict;
+use warnings;
+
+open(my $in1,"$ARGV[0]") or die "$!";  #transcriptomic fasta
+open(my $in2,"$ARGV[1]") or die "$!";  #blast -m 8 table filter awk command
+open(my $out,">filter_result_fasta_1.txt");  #output file
+
+my %fasta=();
+$/=">";
+for(<$in1>){
+        chomp;
+        next if(/^$/);
+        my($gene_id,$seq)=split(/\n/,$_,2);
+        $seq =~s/\n//g;
+        $fasta{$gene_id}=$seq;
+}
+close $in1;
+$/="\n";
+while(<$in2>){
+        chomp;
+        #my $temp=(split /\|/,$_,2)[0];
+        my $temp1=(split /\s+/,$_,2)[0];
+        for my $key(keys %fasta){
+                #my $head=(split /\|/,$key,2)[0];
+                #print "$temp","\n";
+                #if($head eq $temp){
+                if($temp1 eq $key){
+                        print $out ">",$_,"\n";
+                        print $out $fasta{$key},"\n";
+                }
+        }
+}
+close $in2;
+close $out;
+
+__END__
+USAGE:
+blast_result_abstract_3_generate_transcriptomic.pl transcript.fasta filter_blast.txt
